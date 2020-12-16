389f6c92e643c78fe0fd665bb70f6024bcec6a37
Aaron Townsend <atownse17@gmail.com>
2020-12-16 12:55:38 -0600

    Create tests for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('should render with correct info', () => {
+    render (
+      <App />
+    )
+    const header = screen.getByText('Turing Cafe Reservations')
+    expect(header).toBeInTheDocument()
+  });
+
+  it('should be able to add a new reservation', () => {
+    render (
+      <App />
+    )
+
+    const name = screen.getByPlaceholderText('name')
+    const date = screen.getByPlaceholderText('date')
+    const time = screen.getByPlaceholderText('time')
+    const number = screen.getByPlaceholderText('number')
+    const button = screen.getByText('Add Reservation')
+
+    userEvent.type(name, 'Jason Bourne')
+    userEvent.type(date, '11/11')
+    userEvent.type(time, '12:12')
+    userEvent.type(number, '437')
+    userEvent.click(button)
+
+    expect(screen.getByText('Jason Bourne')).toBeInTheDocument()
+    expect(screen.getByText('11/11')).toBeInTheDocument()
+    expect(screen.getByText('12:12 pm')).toBeInTheDocument()
+    expect(screen.getByText('Number of Guests: 437')).toBeInTheDocument()
+  })
+})
